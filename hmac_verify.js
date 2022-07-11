edd8e95802123a3bd8de0847476a7d4a4fd43b7e
Dan Moore <dan@fusionauth.io>
2022-07-11 10:15:33 -0600

    added some business logic checks

--- hmac_verify.js
+++ hmac_verify.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-// the IdP
-const hmac_key = 'hello javascript random STUFF$$!';
+// the user API
+const hmac_key = 'hello';
 const token = jwt.sign(
     { 
-      email: 'test@example.com' ,
-      exp: Math.floor(Date.now() / 1000) + (60 * 60),
+      email: 'dan@example.com' ,
+      exp: Math.floor(Date.now() / 1000) + (5 * 60),
       iss: 'fusionauth.io',
       sub: 'test@example.com',
-      aud: 'myapp.example.com'
-
+      aud: 'myapp.example.com',
+      premiumUser: true
     }
     , hmac_key);
 
@@ -17,7 +17,12 @@ console.log(token);
 
 // the todo api 
 
-const options = {algorithms: "HS256", ignoreExpiration: false, issuer: "fusionauth.io"};
+const options = {
+   algorithms: "HS256", 
+   ignoreExpiration: false, 
+   issuer: "fusionauth.io"
+};
+
 const verified = jwt.verify(token, hmac_key, options);
 
 // addl verification checks
@@ -25,4 +30,8 @@ if (verified.aud != 'myapp.example.com') {
   throw "invalid audience";
 }
 
-console.log(verified);
+if (!verified.premiumUser) {
+  throw "invalid access";
+}
+
+console.log(JSON.stringify(verified));
